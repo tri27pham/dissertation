f9486361b0b3d800d2b77790a7a1bec45b9dedbc
tri27pham <tri.pham2002@outlook.com>
2024-01-25 15:51:00 +0000

    check a task's prior tasks are complete

--- Task Allocation Model/taskAllocator.py
+++ Task Allocation Model/taskAllocator.py
@@ -10,28 +10,41 @@ from task import Task
 class TaskAllocator:
 
     def __init__(self):        
+
         self.highPriorityTasks = []
         self.mediumPriorityTasks = []
         self.lowPriorityTasks = []
 
         # get input of tasks
-        task1 = Task(1,"Task1",2,Priority.HIGH,0,(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.IN)
-        task2 = Task(2,"Task2",1,Priority.MEDIUM,0,(51.513056,-0.117352),TaskType.WORK,InOut.OUT)
-        task3 = Task(3,"Task3",0.5,Priority.HIGH,0,(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.IN)
-        task4 = Task(4,"Task4",2.5,Priority.HIGH,0,(51.513056,-0.117352),TaskType.FITNESS,InOut.OUT)
-        task5 = Task(5,"Task5",2,Priority.MEDIUM,0,(51.513056,-0.117352),TaskType.COOKING,InOut.IN)
-        task6 = Task(6,"Task6",1,Priority.HIGH,0,(51.513056,-0.117352),TaskType.SOCIAL,InOut.OUT)
-        task7 = Task(7,"Task7",1.5,Priority.MEDIUM,0,(51.513056,-0.117352),TaskType.MISCELLANEOUS,InOut.OUT)
-        task8 = Task(8,"Task8",1,Priority.MEDIUM,0,(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.IN)
-        task9 = Task(9,"Task9",1,Priority.MEDIUM,0,(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.OUT)
-        task10 = Task(10,"Task10",10,Priority.LOW,0,(51.513056,-0.117352),TaskType.WORK,InOut.IN)
-        task11 = Task(11,"Task11",3,Priority.LOW,0,(51.513056,-0.117352),TaskType.COOKING,InOut.IN)
+        task1 = Task(1,"Task1",2,Priority.HIGH,[],(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.IN)
+        task2 = Task(2,"Task2",1,Priority.MEDIUM,[],(51.513056,-0.117352),TaskType.WORK,InOut.OUT)
+        task3 = Task(3,"Task3",0.5,Priority.HIGH,[1],(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.IN)
+        task4 = Task(4,"Task4",2.5,Priority.HIGH,[1,3],(51.513056,-0.117352),TaskType.FITNESS,InOut.OUT)
+        task5 = Task(5,"Task5",2,Priority.MEDIUM,[],(51.513056,-0.117352),TaskType.COOKING,InOut.IN)
+        task6 = Task(6,"Task6",1,Priority.HIGH,[],(51.513056,-0.117352),TaskType.SOCIAL,InOut.OUT)
+        task7 = Task(7,"Task7",1.5,Priority.MEDIUM,[],(51.513056,-0.117352),TaskType.MISCELLANEOUS,InOut.OUT)
+        task8 = Task(8,"Task8",1,Priority.MEDIUM,[7],(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.IN)
+        task9 = Task(9,"Task9",1,Priority.MEDIUM,[],(51.513056,-0.117352),TaskType.UNIVERSITY,InOut.OUT)
+        task10 = Task(10,"Task10",10,Priority.LOW,[],(51.513056,-0.117352),TaskType.WORK,InOut.IN)
+        task11 = Task(11,"Task11",3,Priority.LOW,[],(51.513056,-0.117352),TaskType.COOKING,InOut.IN)
 
         taskList = [task1,task2,task3,task4,task5,task6,task7,task8,task9,task10,task11]
 
-        self.sortTasks(taskList)
+        self.sortTasksByPriority(taskList)
+    
+    def getTaskIDs(taskList):
+        taskIDs = []
+        for task in taskList:
+            taskIDs.append(task.getID())
+        return taskIDs
 
-    def sortTasks(self,taskList):
+    def arePriorTasksCompleted(task, completedTasks):
+        for priorTask in task.getPriorTasks():
+            if not priorTask in completedTasks.getTaskIDs():
+                return False
+        return True
+
+    def sortTasksByPriority(self,taskList):
         for task in taskList:
             if task.getPriority() == Priority.HIGH:
                 self.highPriorityTasks.append(task)

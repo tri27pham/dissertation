52db5a56f3b50f8d8993ff6658c84b86272c3d55
tri27pham <tri.pham2002@outlook.com>
2024-03-15 18:06:08 +0000

    half of home page done, bit more done now

--- task_allocation_app/lib/homepage.dart
+++ task_allocation_app/lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'addTasks.dart';
 import 'package:intl/intl.dart';
+// import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -50,27 +51,44 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: MediaQuery.of(context).size.height * 0.12,
         backgroundColor: Colors.transparent,
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Color.fromARGB(255, 70, 70, 70),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0), // Add padding to the leading icon
+          child: IconButton(
+            icon: Icon(Icons.menu),
+            iconSize: 45,
+            onPressed: () {
+              print("MENU PRESSED");
+            },
           ),
-          iconSize: 45,
-          onPressed: () {
-            // Add your search action here
-          },
         ),
+        actions: [
+          Padding(
+            padding:
+                const EdgeInsets.all(8.0), // Add padding to the actions icon
+            child: IconButton(
+              icon: Icon(
+                Icons.account_circle_rounded,
+                color: Color.fromARGB(255, 70, 70, 70),
+              ),
+              iconSize: 70,
+              onPressed: () {
+                // Add your profile action here
+                print('Hello, world!');
+              },
+            ),
+          ),
+        ],
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisSize: MainAxisSize.min,
         children: [
           // Hello {name} message
           Container(
             padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.05,
-                vertical: MediaQuery.of(context).size.height * 0.02),
+                horizontal: MediaQuery.of(context).size.width * 0.05),
             child: Text(
               'Hello Tri',
               style: TextStyle(
@@ -96,58 +114,110 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Row(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.6,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Transform.translate(
-                      offset: Offset(0, -20), // Adjust the offset as needed
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width * 0.06,
-                        ),
-                        child: Text(
-                          date,
-                          style: TextStyle(
-                            fontSize: 85, // Change the font size here
-                            color: Color.fromARGB(255, 80, 78,
-                                78), // Change the color of the text here
-                            fontWeight: FontWeight.w400, // Make the text bold
-                            letterSpacing: -5,
+          Container(
+            height: MediaQuery.of(context).size.height * 0.2,
+            child: Row(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Transform.translate(
+                          offset: Offset(0, 20), // Adjust the offset as needed
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.06,
+                            ),
+                            child: Text(
+                              date,
+                              style: TextStyle(
+                                fontSize: 80, // Change the font size here
+                                color: Color.fromARGB(255, 80, 78,
+                                    78), // Change the color of the text here
+                                fontWeight:
+                                    FontWeight.w400, // Make the text bold
+                                letterSpacing: -3,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    // month
-                    Transform.translate(
-                      offset: Offset(0, -65), // Adjust the offset as needed
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width * 0.06,
-                        ),
-                        child: Text(
-                          month,
-                          style: TextStyle(
-                            fontSize: 90, // Change the font size here
-                            color: Color.fromARGB(255, 80, 78,
-                                78), // Change the color of the text here
-                            fontWeight: FontWeight.w400, // Make the text bold
-                            letterSpacing: -5,
+                        // month
+                        Transform.translate(
+                          offset: Offset(0, -20), // Adjust the offset as needed
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.06,
+                            ),
+                            child: Text(
+                              month,
+                              style: TextStyle(
+                                fontSize: 80, // Change the font size here
+                                color: Color.fromARGB(255, 80, 78,
+                                    78), // Change the color of the text here
+                                fontWeight:
+                                    FontWeight.w400, // Make the text bold
+                                letterSpacing: -3,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  child: VerticalDivider(
+                    thickness: 1, // Set the thickness of the line
+                    width: 1, // Set the width of the line
+                    color: Colors.black, // Set the color of the line
+                  ),
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.only(top: 15),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(
+                      40.0), // Adjust the top left corner radius
+                  topRight: Radius.circular(
+                      40.0), // Adjust the top right corner radius
                 ),
               ),
-              Spacer(),
-            ],
-          ),
-          // date
+              elevation: 10,
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 25),
+                    child: Text(
+                      "TODAY'S TASKS",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 20, // Change the font size here
+                        color: Color.fromARGB(255, 80, 78,
+                            78), // Change the color of the text here
+                        fontWeight: FontWeight.w500, // Make the text bold
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )),
         ],
       ),
     );
